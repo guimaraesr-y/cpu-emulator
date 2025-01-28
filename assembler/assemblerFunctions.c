74b115d19c976791801e806b7d79528d3e93bbc1
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-01-28 00:36:52 -0300

    Add missing bytes to assembler functions

--- assembler/assemblerFunctions.c
+++ assembler/assemblerFunctions.c
@@ -6,7 +6,7 @@ void mov(int r, int value, FILE* outputFile) {
     bytecode[0] = OPCODE_MOV;
     bytecode[1] = r;
     bytecode[2] = value;
-    fwrite(bytecode, sizeof(unsigned char), 3, outputFile);
+    fwrite(bytecode, sizeof(unsigned char), LINE_SIZE, outputFile);
 }
 
 void add(int r1, int r2, FILE* outputFile) {
@@ -14,7 +14,7 @@ void add(int r1, int r2, FILE* outputFile) {
     bytecode[0] = OPCODE_ADD;
     bytecode[1] = r1;
     bytecode[2] = r2;
-    fwrite(bytecode, sizeof(unsigned char), 3, outputFile);
+    fwrite(bytecode, sizeof(unsigned char), LINE_SIZE, outputFile);
 }
 
 void sub(int r1, int r2, FILE* outputFile) {
@@ -22,14 +22,15 @@ void sub(int r1, int r2, FILE* outputFile) {
     bytecode[0] = OPCODE_SUB;
     bytecode[1] = r1;
     bytecode[2] = r2;
-    fwrite(bytecode, sizeof(unsigned char), 3, outputFile);
+    fwrite(bytecode, sizeof(unsigned char), LINE_SIZE, outputFile);
 }
 
 void jmp(int address, FILE* outputFile) {
     unsigned char bytecode[2];
     bytecode[0] = OPCODE_JMP;
     bytecode[1] = address;
-    fwrite(bytecode, sizeof(unsigned char), 2, outputFile);
+    bytecode[2] = 0x00;
+    fwrite(bytecode, sizeof(unsigned char), LINE_SIZE, outputFile);
 }
 
 void xor(int r1, int r2, FILE* outputFile) {
@@ -37,11 +38,13 @@ void xor(int r1, int r2, FILE* outputFile) {
     bytecode[0] = OPCODE_XOR;
     bytecode[1] = r1;
     bytecode[2] = r2;
-    fwrite(bytecode, sizeof(unsigned char), 3, outputFile);
+    fwrite(bytecode, sizeof(unsigned char), LINE_SIZE, outputFile);
 }
 
 void hlt(FILE* outputFile) {
     unsigned char bytecode[1];
     bytecode[0] = OPCODE_HLT;
-    fwrite(bytecode, sizeof(unsigned char), 1, outputFile);
+    bytecode[1] = OPCODE_HLT;
+    bytecode[2] = OPCODE_HLT;
+    fwrite(bytecode, sizeof(unsigned char), LINE_SIZE, outputFile);
 }
